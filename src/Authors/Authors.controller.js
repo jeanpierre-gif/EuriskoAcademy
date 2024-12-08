10c93559a0a7f6188d6809737376c4c7879e607f
jeanpierre-gif <jaypnakhoul@gmail.com>
2024-12-08 21:48:45 +0200

    added validation for author creation

--- src/Authors/Authors.controller.js
+++ src/Authors/Authors.controller.js
@@ -1,15 +1,29 @@
 const Author = require("./Authors.model");
-
+const AuthorValidationSchema = require('../Validators/Authors.validator');
 const createAuthor = async (req, res) => {
   try {
-    const { name, email, biography, birthDate } = req.body;
-    const profileImageUrl = req.file ? req.file.path : null;
+    const {error, value}=AuthorValidationSchema.validate(req.body,{
+        abortEarly:false
+    });
+    if (error) {
+        return res.status(400).json({
+          success: false,
+          message: "Validation error",
+          //mappign to display all the errors
+          details: error.details.map((err) => err.message),
+        });
+      }
+      const existingEmail = await Author.findOne({email:value.email});
+      if (existingEmail) {
+        return res.status(400).json({
+          success: false,
+          message: "author with this email already exists",
+        });
+      }   
+    const profileImageUrl = req.file ;
     const author = new Author({
-      name,
-      email,
-      biography,
-      profileImageUrl,
-      birthDate,
+     ...value,
+      profileImageUrl:profileImageUrl ? profileImageUrl.filename : null,
     });
     console.log(author);
     await author.save();
--- src/Validators/Authors.validator.js
+++ src/Validators/Authors.validator.js
@@ -1,2 +1,43 @@
 const joi = require('joi');
 
+const AuthorValidationSchema = joi.object({
+   name:joi.object({
+    en: joi.string()
+    .pattern(/^[a-zA-Z\s]+$/)
+    .required()
+    .messages({
+        "string.pattern.base": "name is not in english",
+        "string.empty": "name in english is required",
+      }),
+      ar: joi
+      .string()
+      .pattern(/^[\u0600-\u06FF\u0750-\u077F\u08A0-\u08FF\s]+$/)
+      .required()
+      .messages({
+        "string.pattern.base": "name is not in arabic",
+        "string.empty": "name in arabic is required",
+      }), 
+      }),
+      email:joi.string().email().required(),
+      biography:joi.object({
+        en:joi.string()
+        .pattern(/^[a-zA-Z\s]+$/)
+        .required()
+        .messages({
+            "string.pattern.base": "biography is not in english",
+            "string.empty": "biography in english is required",
+          }),
+          ar: joi
+          .string()
+          .pattern(/^[\u0600-\u06FF\u0750-\u077F\u08A0-\u08FF\s]+$/)
+          .required()
+          .messages({
+            "string.pattern.base": "biography is not in arabic",
+            "string.empty": "biography in arabic is required",
+          }), 
+         
+      }),
+      profileImageUrl:joi.string().uri(),
+      birthDate:joi.date()
+})
+module.exports = AuthorValidationSchema;
